249f366ee6f7287b580676c226da4a3c51b68685
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-04-14 11:36:15 +0300

    Анонимный чат

--- Server/src/ru/smak/chat/ConnectedClient.kt
+++ Server/src/ru/smak/chat/ConnectedClient.kt
@@ -5,16 +5,28 @@ import java.net.Socket
 class ConnectedClient(val socket: Socket) {
 
     private val communicator = Communicator(socket)
+    private val userName: String? = null
 
     init{
+        connectedClients.add(this)
         communicator.start{ parse(it) }
     }
 
     private fun parse(message: String){
-        communicator.sendMessage(message)
+        sendToAll(message, echo = false)
     }
 
     fun stop(){
         communicator.stop()
+    }
+
+    fun sendToAll(message: String, echo: Boolean = true){
+        connectedClients.forEach {
+            if (echo || it != this) it.communicator.sendMessage(message)
+        }
+    }
+
+    companion object {
+        private val connectedClients = mutableListOf<ConnectedClient>()
     }
 }
